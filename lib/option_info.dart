b4dda605eedb96a53dd483f776906fc76e10e79e
ggonza55 <ggonza55@uic.edu>
2022-10-17 09:41:42 -0500

    Moved titleStyle into option_info.dart

--- lib/option_info.dart
+++ lib/option_info.dart
@@ -17,3 +17,19 @@ class OptionInfo {
       required this.altDescription,
       required this.swatch});
 }
+
+TextStyle getTitleStyle({double fontSize = 45}) {
+  return TextStyle(
+    color: Colors.white, //Title color
+    height: 3, //Title height
+    fontSize: fontSize, //Title size
+    shadows: const [
+      Shadow(
+          //Title Shadow
+          color: Colors.black,
+          offset: Offset(5, 5),
+          blurRadius: 10)
+    ],
+    fontFamily: "QuickKiss",
+  );
+}
